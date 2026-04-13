767568b350a2425846a0d8fdf2ba2fc07bde7c65
Alessandro Pillajo <alepillajof@gmail.com>
2026-04-13 09:27:41 -0500

    Add files via upload

---
+++ Ejercicio7/src/Principal/Main.java
@@ -0,0 +1,9 @@
+package Principal;
+import modelo.Producto;
+
+public class Main {
+    public static void main(String[] args) {
+        Producto p = new Producto();
+        System.out.println("Nombre del producto: " + p.getNombre());
+    }
+}
---
+++ Ejercicio7/src/modelo/Producto.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class Producto {
+    private String nombre = "Lapiz";
+
+    public String getNombre(){
+        return nombre;
+    }
+}
