dcb40d6ad5134bd18d4034fbe88b9c9789e779dd
Alessandro Pillajo <alepillajof@gmail.com>
2026-04-13 09:26:29 -0500

    Add files via upload

---
+++ Ejercicio5/src/Estudiante.java
@@ -0,0 +1,6 @@
+public class Estudiante {
+    private String nombre;
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+}
---
+++ Ejercicio5/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        Estudiante e = new Estudiante();
+        e.setNombre("Ana");
+    }
+}
